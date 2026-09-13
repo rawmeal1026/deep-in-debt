d2510475913aad1f3553836d04919dd14eed3d15
RawMeal <rawmeal1026@gmail.com>
2026-09-14 01:42:32 +0800

    Revert "feat (Audio): implemented volume change functions and audio buses"
    This reverts commit ba061e5b66051e74342a258cdfadc3c5d162034b.

--- deep-in-debt/scripts/audio_manager.gd
+++ deep-in-debt/scripts/audio_manager.gd
@@ -21,7 +21,7 @@ extends Node
 func play_sfx_oneshot(node: String, pitch: float = -1.0):
 	var event = get(node)
 	if event == null:
-		push_warning("audio_manager.gd/play_sfx_oneshot(): WARN! sound event not found (requested oneshot event: "+node+")")
+		push_warning("audio_manager.gd/play_sfx_oneshot(): WARN! sound event not found ('"+node+"' doesn't exist!)")
 		return
 	if pitch != -1:
 		event.pitch_scale = pitch
@@ -31,7 +31,7 @@ func play_sfx_oneshot(node: String, pitch: float = -1.0):
 func play_or_update_loop(node_name: String, clip_id: int = 0) -> void:
 	var event = get(node_name)
 	if event == null:
-		push_warning("audio_manager.gd/play_or_update_loop(): WARN! loop event not found (requested loop event: " + node_name + ")")
+		push_warning("audio_manager.gd/play_or_update_loop(): WARN! loop event not found ('" + node_name + "')")
 		return
 	
 	if event is AudioStreamPlayer:
@@ -42,20 +42,3 @@ func play_or_update_loop(node_name: String, clip_id: int = 0) -> void:
 		if playback is AudioStreamPlaybackInteractive:
 			if playback.get_current_clip_index() != clip_id:
 				playback.switch_to_clip(clip_id)
-
-func change_volume(volume, audio_bus: String = "Master"):
-	#audio_bus is Master by default, it can be:
-	# 1. "Master" -> for the general volume (affects all the other buses too)
-	# 2. "Voices" -> for the npc voices
-	# 3. "Sfx" -> for pickup, put down and walking sounds
-	# 4. "Music" -> for the background music
-	# The volume parameter is expressed in Db and can go from -80 to 24 (0 is normal level)
-	
-	var sfx_index= AudioServer.get_bus_index(audio_bus)
-	if sfx_index == null:
-		push_warning("audio_manager.gd/change_volume(): WARN! bus not found (Requested bus: " + audio_bus + ")")
-		return
-	if volume < -80 or volume > 24:
-		push_warning("audio_manager.gd/change_volume(): WARN! Volume must be between -80Db and 24Db (requested volume: " + str(volume) + ")")
-		return
-	AudioServer.set_bus_volume_db(sfx_index, volume)
